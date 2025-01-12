8f9471d3a3c0503944f2af86b7fc1b2958b3dfb3
YouXam <youxam@outlook.com>
2025-01-12 17:09:50 +0800

    feat: add postgresql support (#12)
    * feat: add postgresql support
    
    Signed-off-by: YouXam <youxam@outlook.com>
    
    * Update gorm log level
    
    Signed-off-by: YouXam <youxam@outlook.com>
    
    ---------
    
    Signed-off-by: YouXam <youxam@outlook.com>

---
+++ database/postgresql.go
@@ -0,0 +1,40 @@
+package database
+
+import (
+	"domain0/config"
+	"fmt"
+	"github.com/sirupsen/logrus"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+	"os"
+)
+
+func postgresqlInit(c config.Config) (*gorm.DB, error) {
+	timezone, exist := os.LookupEnv("TZ")
+	if !exist {
+		timezone = "UTC"
+	}
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%d TimeZone=%s",
+		c.Database.Host,
+		c.Database.Username,
+		c.Database.Password,
+		c.Database.DbName,
+		c.Database.Port,
+		timezone,
+	)
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		logrus.Errorf("failed to connect to PostgreSQL: %v", err)
+		return nil, err
+	}
+
+	err = migrate(db)
+	if err != nil {
+		logrus.Errorf("migration failed: %v", err)
+		return nil, err
+	}
+
+	return db, nil
+}
